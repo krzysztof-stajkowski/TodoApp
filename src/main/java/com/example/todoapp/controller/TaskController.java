0fc57bdb0dfb533d66e39ee12b36b209b8ed1e62
ksx <kd.stajkowski@gmail.com>
2022-06-24 16:21:15 +0200

    Task Controller ResponseEntity

--- src/main/java/com/example/todoapp/controller/TaskController.java
+++ src/main/java/com/example/todoapp/controller/TaskController.java
@@ -1,16 +1,27 @@
 package com.example.todoapp.controller;
 
+
 import com.example.todoapp.model.TaskRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RepositoryRestController
- class TaskController {
+class TaskController {
     public static final Logger logger = LoggerFactory.getLogger(TaskController.class);
     private final TaskRepository repository; //aby to działało z poza pakietu model musi być TaskRepository interfejs public
-//kiedyś trzeba było dodawać @Autowired ale nie jest to już potrzebne
+
+    //kiedyś trzeba było dodawać @Autowired ale nie jest to już potrzebne
     TaskController(final TaskRepository repository) {
         this.repository = repository;
     }
+
+    @GetMapping("/tasks") //dzieki temu nadpisaniu akcje na /tasks będą miały prostrzy zapis w postmanie ale bez HATEOAS i dodatkowo info w loggerze
+    ResponseEntity<?> readAllTasks() {
+        logger.warn("Exposing all the tasks!");
+        return ResponseEntity.ok(repository.findAll());
+    }
+
 }
