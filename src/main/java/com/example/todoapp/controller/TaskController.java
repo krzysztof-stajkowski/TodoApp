68ab74db7e050d5aa2f6fcfcab3075c97e502be8
ksx <kd.stajkowski@gmail.com>
2022-06-25 16:50:47 +0200

    Putmapping

--- src/main/java/com/example/todoapp/controller/TaskController.java
+++ src/main/java/com/example/todoapp/controller/TaskController.java
@@ -8,8 +8,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import java.util.List;
 
 
@@ -36,6 +39,12 @@ class TaskController {
     ResponseEntity<List<Task>> readAllTasks(Pageable page) {
         logger.info("Custom pageable");
         return ResponseEntity.ok(repository.findAll(page).getContent()); //trzeba aby nie było błędu pobrac GetContent bo page nie jest listą
+    }
+
+    @PutMapping("tasks/{id}")
+    ResponseEntity<?> updateTask(@RequestBody Task toUpdate){
+        repository.save(toUpdate);
+        return ResponseEntity.noContent().build();
     }
 
 }
